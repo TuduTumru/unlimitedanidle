71d3c85393b913cd7fa39ea06dda98a40ef94280
TuduTumru <tudutumru@gmail.com>
2024-12-10 23:43:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.getElementById("submitguess").onclick = function updatecounter(){
       else {
         let mysteryanime = data[randomnumber];
         console.log('Mystery anime is:',mysteryanime.Name);
-        img.src = `Pics/${Number(mysteryanime.Popularity)}.jpg`
+        img.src = `${Number(mysteryanime.Popularity)}.jpg`
         const titles = document.getElementsByClassName("animetitle");
         globalsearchresult[0].English_name == "Unknown" ? titles[16-guesses].textContent = globalsearchresult[0].Name : titles[16-guesses].textContent = globalsearchresult[0].English_name;
         const score = document.getElementsByClassName("score");
